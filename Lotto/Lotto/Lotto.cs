d28f4d0af109a9ccea2be0a5e79221eac0bd8192
Bibushka <bianca_bucur03@yahoo.com>
2017-11-21 21:42:34 +0200

    Lotto test: red

---
+++ Lotto/Lotto/Lotto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lotto
+{
+    [TestClass]
+    public class Lotto
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 7, 19, 27, 28, 34 }, OrderLottoNumbers(new int[] { 1, 27, 28 }, new int[] { 7, 19, 34 }));
+        }
+
+        public int[] OrderLottoNumbers(int[] originalLine, int[] newLine)
+        {
+            return new int[] {0};
+        }
+
+    }
+}
