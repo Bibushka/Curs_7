4f4d589fda80c1ff42f7018f762a9d7e97cd192a
Bibushka <bianca_bucur03@yahoo.com>
2017-11-23 00:13:32 +0200

    Priorities test: red

---
+++ Priorities/Priorities/Priorities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Priorities
+{
+    [TestClass]
+    public class Priorities
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(new string[] { "H", "H", "M", "M", "M", "M", "L", "L", "L"}, 
+                SortPriority(new string[] {"M", "L", "M", "L", "H", "L", "M", "H", "M" }));
+        }
+
+        public string[] SortPriority(string[] originalString)
+        {
+            return new string[] {"0", "0"};
+        }
+    }
+}
